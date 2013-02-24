27730ca961f034b7d6cafcc3192e9f8fd07f8cee
Chen Zihui <hello@chenzihui.com>
2013-02-24 20:13:57 +0800

    Add form validation for registration

--- client/register.js
+++ client/register.js
@@ -4,6 +4,9 @@
  * Handles user registration for new accounts
  **/
 
+// Event Handling
+// ------------------------------
+
 Template.register.events = {
   'submit #reg-form': function( evt ) {
     evt.preventDefault();
@@ -15,13 +18,63 @@ Template.register.events = {
       password: $( '#reg-pw' ).val()
     };
 
-    Accounts.createUser(options, function( err ) {
-      if ( err ) {
-        console.log(err);
-      }
-      else {
-        console.log('User created');
-      }
-    });
+    // Form validation
+    var errorMsgs = validateReg( options );
+
+    if( _.isEmpty(errorMsgs) ) {
+      Session.set( 'regError', undefined );
+
+      Accounts.createUser( options, function( err ) {
+        console.log('Account created');
+      });
+    } else {
+      Session.set( 'regError', errorMsgs );
+    }
   }
+};
+
+// Template Helpers
+// ------------------------------
+
+Template.register.errors = function() {
+  return Session.get( 'regError' );
+};
+
+/*
+ * Validates the registration form
+ *
+ * @method validateReg
+ *
+ * @param {Object} - User input
+ *
+ * @return {Object} - Error messages, if any
+ */
+
+var validateReg = function( values ) {
+  var errorMsgs  = [],
+      emailRegex = /^[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$/,
+      passRegex  = /^[a-zA-Z0-9]{6,}/;
+
+  // Check that no fields are empty
+  for ( key in values ) {
+    if ( values.hasOwnProperty( key ) ) {
+      if ( values[key].length === 0 ) {
+        errorMsgs.push( 'All fields must be filled up' );
+        break;
+      }
+    }
+  }
+
+  // Check that the email provided is valid
+  if ( !emailRegex.test( values.email ) ) {
+    errorMsgs.push( 'Please enter a valid email address' );
+  }
+
+  // Check that passwords contain at least 6 alphanumeric characters
+  if ( !passRegex.test( values.password ) ) {
+    errorMsgs.push( 'Please provide at least 6 alphanumeric characters ' +
+        'for your password' );
+  }
+
+  return errorMsgs;
 };
